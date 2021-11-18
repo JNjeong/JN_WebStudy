cef54faf6772b68e9e8c3c61e1d7e663ca45800c
JNjeong <wnsghd0902@gmail.com>
2021-11-19 01:04:23 +0900

    week11 commit

---
+++ Web_week_11/JavaScript_day06/WebContent/script.js
@@ -0,0 +1,10 @@
+/**
+ * hello.html의 javaScript
+ */
+
+window.onload = function(){
+	let hw = document.getElementById("hw");
+	hw.addEventListener("click", function(e) {
+		alert("hello");
+	});
+}
